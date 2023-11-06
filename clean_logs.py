9ced208755f17c00497b8b573ce7eef29eee25d4
Nu55k0pF <madironbrain@googlemail.com>
2023-11-06 10:59:18 +0100

    now can scan folder with logs

--- clean_logs.py
+++ clean_logs.py
@@ -2,9 +2,13 @@
 Explenation goes here
 '''
 
+import os
+import shutil
+
+
 def read_log (file: str) -> list:
 
-    with open(file, 'r') as f:
+    with open(file, 'r', encoding='ANSI') as f:
         lines = f.readlines()
         lines_to_delete = []
         i = 0
@@ -28,18 +32,40 @@ def read_log (file: str) -> list:
 
 
 def clean_log(lines: list, list_of_lines: list, file: str):
-    with open(file, 'w') as f:
+    with open(file, 'w', encoding='ANSI') as f:
         for number, line in enumerate(lines):
             if number not in list_of_lines:
                 f.write(line)
 
 
+def copy_file_with_old(filename):
+    # get the file name and extension
+    name, ext = os.path.splitext(filename)
+    # create a new file name with "_old" suffix
+    new_name = name + "_old" + ext
+    # copy the original file to the new file
+    shutil.copyfile(filename, new_name)
+
+
 def main():
 
-    testfile = 'E:/Python Tools/Clean Logs/clean_logs/28102023Classix.asc'
+    log_folder = 'E:/Python Tools/Clean Logs/clean_logs/logs'
+    log_files = os.listdir('clean_logs\logs')
+    
+    for log_file in log_files:
 
-    lines, lines_to_delete = read_log(testfile)
-    clean_log(lines, lines_to_delete, testfile)
+        # Read in the log file to clean
+        path = log_folder + '/' + log_file
+        lines, lines_to_delete = read_log(path)
+
+        if len(lines_to_delete) == 0:
+            # if there are no duplicate lines do nothing
+            continue
+        else:
+            # make a backup of the old logfile
+            copy_file_with_old(path)
+            # write new logfile with duplicate lines removed
+            clean_log(lines, lines_to_delete, path)
 
 
 
